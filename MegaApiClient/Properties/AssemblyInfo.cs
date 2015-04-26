ad23a0aedef703a42bd1b9a98321d809a39c7d90
Gregoire Pailler <gregoire@pailler.fr>
2015-04-26 15:15:25 +0200

    Added .net 4.0 target

--- MegaApiClient/Properties/AssemblyInfo.cs
+++ MegaApiClient/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Grégoire Pailler")]
 [assembly: AssemblyProduct("MegaApiClient")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
