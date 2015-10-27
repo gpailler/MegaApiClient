74da81e310a0fb8d46ad2c65f7179282d6d2827b
Gregoire Pailler <gregoire@pailler.fr>
2015-10-27 23:42:08 +0800

    Allowed untrusted certificate to allow Fiddler interception

--- MegaApiClient.Tests/FiddlerProxy.cs
+++ MegaApiClient.Tests/FiddlerProxy.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Fiddler;
 using NUnit.Framework;
@@ -13,6 +16,8 @@ namespace CG.Web.MegaApiClient.Tests
         [SetUp]
         public void Setup()
         {
+            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
+
             FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
             FiddlerApplication.Startup(1234, FiddlerCoreStartupFlags.Default);
         }
