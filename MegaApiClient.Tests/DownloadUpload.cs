gpailler/MegaApiClient

commit f11beb0793dc81d4ce06a312eac40c2565363112

Fix #13. Sharing data was not sent during file or folder upload. File or folder uploaded on a shared folder was only visible with owner account and not visible using public share access.

MegaApiClient/INode.cs
@@ -20,10 +20,10 @@ namespace CG.Web.MegaApiClient
     }
 
     internal interface INodeCrypto
-    { 
-        byte[] DecryptedKey { get; }
-
+    {
         byte[] Key { get; }
+
+        byte[] SharedKey { get; }
 
         byte[] Iv { get; }
 

MegaApiClient/MegaApiClient.cs
@@ -328,7 +328,7 @@ namespace CG.Web.MegaApiClient
             byte[] attributes = Crypto.EncryptAttributes(new Attributes(name), key);
             byte[] encryptedKey = Crypto.EncryptAes(key, this._masterKey);
 
-            CreateNodeRequest request = CreateNodeRequest.CreateFolderNodeRequest(parent, attributes.ToBase64(), encryptedKey.ToBase64());
+            CreateNodeRequest request = CreateNodeRequest.CreateFolderNodeRequest(parent, attributes.ToBase64(), encryptedKey.ToBase64(), key);
             GetNodesResponse response = this.Request<GetNodesResponse>(request, this._masterKey);
             return response.Nodes[0];
         }
@@ -369,7 +369,7 @@ namespace CG.Web.MegaApiClient
                 "/#{0}!{1}!{2}",
                 node.Type == NodeType.Directory ? "F" : string.Empty,
                 response,
-                nodeCrypto.DecryptedKey.ToBase64()));
+                nodeCrypto.Key.ToBase64()));
         }
 
         /// <summary>
@@ -604,7 +604,7 @@ namespace CG.Web.MegaApiClient
 
                 byte[] encryptedKey = Crypto.EncryptKey(fileKey, this._masterKey);
 
-                CreateNodeRequest createNodeRequest = CreateNodeRequest.CreateFileNodeRequest(parent, cryptedAttributes.ToBase64(), encryptedKey.ToBase64(), completionHandle);
+                CreateNodeRequest createNodeRequest = CreateNodeRequest.CreateFileNodeRequest(parent, cryptedAttributes.ToBase64(), encryptedKey.ToBase64(), fileKey, completionHandle);
                 GetNodesResponse createNodeResponse = this.Request<GetNodesResponse>(createNodeRequest, this._masterKey);
                 return createNodeResponse.Nodes[0];
             }

MegaApiClient/Node.cs
@@ -48,10 +48,10 @@ namespace CG.Web.MegaApiClient
         public DateTime LastModificationDate { get; private set; }
 
         [JsonIgnore]
-        public byte[] DecryptedKey { get; private set; }
+        public byte[] Key { get; private set; }
 
         [JsonIgnore]
-        public byte[] Key { get; private set; }
+        public byte[] SharedKey { get; private set; }
 
         [JsonIgnore]
         public byte[] Iv { get; private set; }
@@ -101,9 +101,6 @@ namespace CG.Web.MegaApiClient
                     int splitPosition = serializedKey.IndexOf(":", StringComparison.InvariantCulture);
                     byte[] encryptedKey = serializedKey.Substring(splitPosition + 1).FromBase64();
 
-                    this.DecryptedKey = Crypto.DecryptKey(encryptedKey, masterKey);
-                    this.Key = this.DecryptedKey;
-
                     // If node is shared, we need to retrieve shared masterkey
                     if (nodesResponse.SharedKeys != null)
                     {
@@ -114,21 +111,21 @@ namespace CG.Web.MegaApiClient
                             masterKey = Crypto.DecryptKey(sharedKey.Key.FromBase64(), masterKey);
                             if (this.Type == NodeType.Directory)
                             {
-                                this.DecryptedKey = masterKey;
+                                this.SharedKey = masterKey;
                             }
                             else
                             {
-                                this.DecryptedKey = Crypto.DecryptKey(encryptedKey, masterKey);
+                                this.SharedKey = Crypto.DecryptKey(encryptedKey, masterKey);
                             }
-
-                            this.Key = Crypto.DecryptKey(encryptedKey, masterKey);
                         }
                     }
+
+                    this.Key = Crypto.DecryptKey(encryptedKey, masterKey);
 
                     if (this.Type == NodeType.File)
                     {
                         byte[] iv, metaMac, fileKey;
-                        Crypto.GetPartsFromDecryptedKey(this.DecryptedKey, out iv, out metaMac, out fileKey);
+                        Crypto.GetPartsFromDecryptedKey(this.Key, out iv, out metaMac, out fileKey);
 
                         this.Iv = iv;
                         this.MetaMac = metaMac;

MegaApiClient/Crypto.cs
@@ -33,14 +33,19 @@ namespace CG.Web.MegaApiClient
 {
     internal class Crypto
     {
-        private static readonly Rijndael Rijndael;
+        private static readonly Rijndael RijndaelCbc;
+        private static readonly Rijndael RijndaelEcb;
         private static readonly byte[] DefaultIv = new byte[16];
 
         static Crypto()
         {
-            Rijndael = Rijndael.Create();
-            Rijndael.Padding = PaddingMode.None;
-            Rijndael.Mode = CipherMode.CBC;
+            RijndaelCbc = Rijndael.Create();
+            RijndaelCbc.Padding = PaddingMode.None;
+            RijndaelCbc.Mode = CipherMode.CBC;
+
+            RijndaelEcb = Rijndael.Create();
+            RijndaelEcb.Padding = PaddingMode.None;
+            RijndaelEcb.Mode = CipherMode.ECB;
         }
 
         #region Key
@@ -94,7 +99,7 @@ namespace CG.Web.MegaApiClient
 
         public static byte[] DecryptAes(byte[] data, byte[] key)
         {
-            using (ICryptoTransform decryptor = Rijndael.CreateDecryptor(key, DefaultIv))
+            using (ICryptoTransform decryptor = RijndaelCbc.CreateDecryptor(key, DefaultIv))
             {
                 return decryptor.TransformFinalBlock(data, 0, data.Length);
             }
@@ -102,7 +107,7 @@ namespace CG.Web.MegaApiClient
 
         public static byte[] EncryptAes(byte[] data, byte[] key)
         {
-            using (ICryptoTransform encryptor = Rijndael.CreateEncryptor(key, DefaultIv))
+            using (ICryptoTransform encryptor = RijndaelCbc.CreateEncryptor(key, DefaultIv))
             {
                 return encryptor.TransformFinalBlock(data, 0, data.Length);
             }
@@ -119,7 +124,15 @@ namespace CG.Web.MegaApiClient
                 return rijndael.Key;
             }
         }
-        
+
+        public static byte[] EncryptAesEcb(byte[] data, byte[] key)
+        {
+            using (ICryptoTransform encryptor = RijndaelEcb.CreateEncryptor(key, DefaultIv))
+            {
+                return encryptor.TransformFinalBlock(data, 0, data.Length);
+            }
+        }
+
         #endregion
 
         #region Attributes

MegaApiClient/JsonSerialization.cs
@@ -25,6 +25,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #endregion
 
+using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
@@ -195,34 +196,103 @@ namespace CG.Web.MegaApiClient
 
     internal class CreateNodeRequest : RequestBase
     {
-        private CreateNodeRequest(INode parentNode, NodeType type, string attributes, string key, string completionHandle)
+        private CreateNodeRequest(INode parentNode, NodeType type, string attributes, string encryptedKey, byte[] key, string completionHandle)
             : base("p")
         {
             this.ParentId = parentNode.Id;
-            this.Nodes = new []
+            this.Nodes = new[]
+            {
+                new CreateNodeRequestData
                 {
-                    new CreateNodeRequestData
-                        {
-                            Attributes = attributes,
-                            Key = key,
-                            Type = type,
-                            CompletionHandle = completionHandle
-                        }
-                };
+                    Attributes = attributes,
+                    Key = encryptedKey,
+                    Type = type,
+                    CompletionHandle = completionHandle
+                }
+            };
+
+            this.Share = new ShareData
+            {
+                CompletionHandle = completionHandle,
+                Parent = parentNode,
+                Key = key
+            };
         }
 
-        public static CreateNodeRequest CreateFileNodeRequest(INode parentNode, string attributes, string key, string completionHandle)
+        public static CreateNodeRequest CreateFileNodeRequest(INode parentNode, string attributes, string encryptedkey, byte[] fileKey, string completionHandle)
         {
-            return new CreateNodeRequest(parentNode, NodeType.File, attributes, key, completionHandle);
+            return new CreateNodeRequest(parentNode, NodeType.File, attributes, encryptedkey, fileKey, completionHandle);
         }
 
-        public static CreateNodeRequest CreateFolderNodeRequest(INode parentNode, string attributes, string key)
+        public static CreateNodeRequest CreateFolderNodeRequest(INode parentNode, string attributes, string encryptedkey, byte[] key)
         {
-            return new CreateNodeRequest(parentNode, NodeType.Directory, attributes, key, "xxxxxxxx");
+            return new CreateNodeRequest(parentNode, NodeType.Directory, attributes, encryptedkey, key, "xxxxxxxx");
         }
 
         [JsonProperty("t")]
         public string ParentId { get; private set; }
+
+        [JsonProperty("cr")]
+        public ShareData Share { get; private set; }
+
+        [JsonConverter(typeof(ShareDataConverter))]
+        internal class ShareData
+        {
+            public string CompletionHandle { get; set; }
+
+            public INode Parent { get; set; }
+
+            public byte[] Key { get; set; }
+        }
+
+        private class ShareDataConverter : JsonConverter
+        {
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                ShareData data = value as ShareData;
+                if (data == null)
+                {
+                    throw new ArgumentException("invalid data to serialize");
+                }
+
+                INodeCrypto parent = data.Parent as INodeCrypto;
+                if (parent == null)
+                {
+                    throw new ArgumentException("parent node must implement INodeCrypto");
+                }
+                
+                writer.WriteStartArray();
+
+                if (parent.SharedKey != null)
+                {
+                    writer.WriteStartArray();
+                    writer.WriteValue(data.Parent.Id);
+                    writer.WriteEndArray();
+
+                    writer.WriteStartArray();
+                    writer.WriteValue(data.CompletionHandle);
+                    writer.WriteEndArray();
+
+                    writer.WriteStartArray();
+                    writer.WriteValue(0);
+                    writer.WriteValue(0);
+                    writer.WriteValue(Crypto.EncryptAesEcb(data.Key, parent.SharedKey).ToBase64());
+                    writer.WriteEndArray();
+                }
+
+                writer.WriteEndArray();
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType == typeof(ShareData);
+            }
+        }
 
         [JsonProperty("n")]
         public CreateNodeRequestData[] Nodes { get; private set; }

commit 79a2e878b5650028344a0365860189ba93df91c6

Fixed #18. Download link used wrong key data (regression introduced by f11beb0793dc81d4ce06a312eac40c2565363112)

MegaApiClient.Tests/DownloadUpload.cs
@@ -48,6 +48,26 @@ namespace CG.Web.MegaApiClient.Tests
             Assert.That(node.Size, Is.EqualTo(data.Length));
             Assert.That(node, Is.EqualTo(this.Client.GetNodes().Single(x => x.Id == node.Id)));
         }
+        
+        [Test]
+        public void UploadStream_DownloadLink_Succeeds()
+        {
+            byte[] data = new byte[123];
+            this.random.NextBytes(data);
+
+            INode parent = this.GetNode(NodeType.Root);
+
+            INode node;
+            using (Stream stream = new MemoryStream(data))
+            {
+                node = this.Client.Upload(stream, "test", parent);
+
+                Uri uri = this.Client.GetDownloadLink(node);
+
+                stream.Position = 0;
+                this.AreStreamsEquals(this.Client.Download(uri), stream);
+            }
+        }
 
         [TestCase(20000)] // 1 chunk
         [TestCase(200000)] // 2 chunks

MegaApiClient/INode.cs
@@ -28,6 +28,8 @@ namespace CG.Web.MegaApiClient
         byte[] Iv { get; }
 
         byte[] MetaMac { get; }
+
+        byte[] FullKey { get; }
     }
 
     public enum NodeType

MegaApiClient/MegaApiClient.cs
@@ -369,7 +369,7 @@ namespace CG.Web.MegaApiClient
                 "/#{0}!{1}!{2}",
                 node.Type == NodeType.Directory ? "F" : string.Empty,
                 response,
-                nodeCrypto.Key.ToBase64()));
+                nodeCrypto.FullKey.ToBase64()));
         }
 
         /// <summary>

MegaApiClient/Node.cs
@@ -51,6 +51,9 @@ namespace CG.Web.MegaApiClient
         public byte[] Key { get; private set; }
 
         [JsonIgnore]
+        public byte[] FullKey { get; private set; }
+
+        [JsonIgnore]
         public byte[] SharedKey { get; private set; }
 
         [JsonIgnore]
@@ -120,16 +123,20 @@ namespace CG.Web.MegaApiClient
                         }
                     }
 
-                    this.Key = Crypto.DecryptKey(encryptedKey, masterKey);
+                    this.FullKey = Crypto.DecryptKey(encryptedKey, masterKey);
 
                     if (this.Type == NodeType.File)
                     {
                         byte[] iv, metaMac, fileKey;
-                        Crypto.GetPartsFromDecryptedKey(this.Key, out iv, out metaMac, out fileKey);
+                        Crypto.GetPartsFromDecryptedKey(this.FullKey, out iv, out metaMac, out fileKey);
 
                         this.Iv = iv;
                         this.MetaMac = metaMac;
                         this.Key = fileKey;
+                    }
+                    else
+                    {
+                        this.Key = this.FullKey;
                     }
 
                     Attributes attributes = Crypto.DecryptAttributes(this.SerializedAttributes.FromBase64(), this.Key);