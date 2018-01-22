137d866c10c8720ec61c7251860aa3b94c79f761
Gregoire Pailler <gregoire@pailler.fr>
2018-01-22 10:53:09 +0700

    Added script to push docs to gh-pages branch

---
+++ docs/pushDoc.ps1
@@ -0,0 +1,46 @@
+# Based on https://github.com/johnnyreilly/jQuery.Validation.Unobtrusive.Native/
+
+param([string]$buildFolder, [string]$email, [string]$username, [string]$personalAccessToken)
+
+if ($true -Or $env:APPVEYOR_REPO_BRANCH -eq "master" -And $env:APPVEYOR_REPO_TAG -eq $true)
+{
+  Write-Host "- Set config settings...."
+  git config --global user.email $email
+  git config --global user.name $username
+  git config --global push.default matching
+
+  Write-Host "- Clone gh-pages branch...."
+  cd "$($buildFolder)\..\"
+  mkdir gh-pages
+  git clone --quiet --branch=gh-pages https://$($username):$($personalAccessToken)@github.com/$($env:APPVEYOR_REPO_NAME).git .\gh-pages\
+  cd gh-pages
+  git status
+
+  Write-Host "- Clean gh-pages folder...."
+  Get-ChildItem -Attributes !r | Remove-Item -Recurse -Force
+
+  Write-Host "- Copy contents of docs\_site folder into gh-pages folder...."
+  Copy-Item -path "$($buildFolder)\docs\_site\*" -Destination $pwd.Path -Recurse
+
+  git status
+  $thereAreChanges = git status | select-string -pattern "Changes not staged for commit:","Untracked files:" -simplematch
+  if ($thereAreChanges -ne $null)
+  {
+    Write-host "- Committing changes to documentation..."
+    git add --all
+    git status
+    git commit -m "skip ci - static site regeneration"
+    git status
+    #Write-Host "- Push it...."
+    #git push --quiet
+    #Write-Host "- Pushed it good!"
+  }
+  else
+  {
+    Write-Host "- No changes to documentation to commit"
+  }
+}
+else
+{
+  Write-Host "Nothing to do. Doc is pushed only on tagged master commit"
+}
